fc3f2c953193ee671b179a4ec7faf4520f821c02
dufenshmirtz <f.chrysomallis@gmail.com>
2024-10-13 23:19:33 +0300

    Polishing new game mechanix
    random character remains until gameend except if you restart in first round so you can randomize again. fixed bugs added round indicators and upgraded functionality

--- Head-of-Hell/Assets/scripts/GameManager.cs
+++ Head-of-Hell/Assets/scripts/GameManager.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,11 +14,19 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI finalWinner;
     string p1, p2;
     static int roundNumber;
-    static int roundCounter=1;
+    static int roundCounter = 1;
     public CharacterManager p1Manager, p2Manager;
     public GameObject playAgainButton;
     public GameObject mainMenuButton;
     public AudioManager audioManager;
+    public GameObject p1R1, p1R2, p1R3;
+    public GameObject p2R1, p2R2, p2R3;
+    static int p1Rounds = 0, p2Rounds = 0;
+    bool tie = false;
+    static string c1Name,c2Name;
+    static bool p1Random = false;
+    static bool p2Random = false;
+    bool gameEnd = false;
 
     // Start is called before the first frame update
     void Start()
@@ -46,13 +52,14 @@ public class GameManager : MonoBehaviour
             // Convert the JSON string back to a CustomRuleset object
             CustomRuleset loadedRuleset = JsonUtility.FromJson<CustomRuleset>(json);
 
-            roundNumber=loadedRuleset.rounds;
+            roundNumber = loadedRuleset.rounds;
         }
         else
         {
             Debug.LogWarning("No ruleset found in PlayerPrefs.");
         }
 
+        ActivateIndicators();
     }
 
     void Awake()
@@ -64,21 +71,37 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void RoundEnd(int playerNum,string winnerName)
+    public void RoundEnd(int playerNum, string winnerName)
     {
         winner.gameObject.SetActive(true);
         DisableGamePlay();
-
         winner.text = winnerName + " prevails!";
 
-        StartCoroutine(WaitAndCallShortWins(playerNum, winnerName));
+        ShortWins(playerNum, winnerName);
+
+        StartCoroutine(WaitAndCheck(playerNum, winnerName));
     }
 
-    public void RoundEndTie()
+    public void RoundEndTie(int playerNum)
     {
+
         winner.gameObject.SetActive(true);
         DisableGamePlay();
         winner.text = "Tie?\nDEATH PREVAILS...";
+        tie = true;
+        if (playerNum == 1)
+        {
+            player1Wins--;
+        }
+        else
+        {
+            player2Wins--;
+        }
+
+        ActivateIndicators();
+        CheckForRandomCharacters();
+        StartCoroutine(WaitAndrestart());
+
     }
 
     public void RoundEndFlawless(int playerNum, string winnerName)
@@ -87,10 +110,12 @@ public class GameManager : MonoBehaviour
         DisableGamePlay();
         winner.text = "FLAWLESS\n" + winnerName + " prevails!";
 
-        StartCoroutine(WaitAndCallShortWins(playerNum, winnerName));
+        ShortWins(playerNum, winnerName);
+
+        StartCoroutine(WaitAndCheck(playerNum, winnerName));
     }
 
-    public void ShortWins(int playerNum,string winnerName)
+    public void ShortWins(int playerNum, string winnerName)
     {
         if (playerNum == 1)
         {
@@ -101,7 +126,19 @@ public class GameManager : MonoBehaviour
             player1Wins++;
         }
 
-        if(player1Wins > roundNumber/2 || player2Wins > roundNumber / 2)
+        ActivateIndicators();
+
+    }
+
+    public void CheckForEnd(int playerNum, string winnerName)
+    {
+        if (tie)
+        {
+            tie = false;
+            return;
+        }
+
+        if (player1Wins > roundNumber / 2 || player2Wins > roundNumber / 2)
         {
             finalWinner.text = "Victory belongs to " + winnerName + "!\n Chan Chan smiles...";
             winner.gameObject.SetActive(false);
@@ -112,25 +149,43 @@ public class GameManager : MonoBehaviour
 
             audioManager.PlaySFX(audioManager.dramaticDrums, audioManager.doubleVol);
 
+            gameEnd = true;
+
             playAgainButton.SetActive(true);
             mainMenuButton.SetActive(true);
+
+            CheckForRandomCharacters();
         }
         else
         {
+            CheckForRandomCharacters();
             roundCounter++;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
-        
     }
 
-    private IEnumerator WaitAndCallShortWins(int playerNum, string winnerName)
+
+
+    private IEnumerator WaitAndCheck(int playerNum, string winnerName)
     {
         // Wait for 3 seconds
         yield return new WaitForSeconds(3f);
 
+
         // Call the ShortWins method after the delay
-        ShortWins(playerNum, winnerName);
+        CheckForEnd(playerNum, winnerName);
+    }
+
+    private IEnumerator WaitAndrestart()
+    {
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(3f);
+
+        tie = false;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 
     public int GetRoundCounter()
@@ -150,10 +205,66 @@ public class GameManager : MonoBehaviour
         p2Manager.Pause();
     }
 
+    void ActivateIndicators()
+    {
+        if (player1Wins == 1)
+        {
+            p1R1.SetActive(true);
+        }
+        if (player2Wins == 1)
+        {
+            p2R1.SetActive(true);
+        }
+        if (player1Wins == 2)
+        {
+            p1R2.SetActive(true);
+        }
+        if (player2Wins == 2)
+        {
+            p2R2.SetActive(true);
+        }
+        if (player1Wins == 3)
+        {
+            p1R3.SetActive(true);
+        }
+        if (player2Wins == 3)
+        {
+            p2R3.SetActive(true);
+        }
+    }
+
+    public void CheckForRandomCharacters()
+    {
+        print(roundCounter);
+        if(PlayerPrefs.GetString("Player1Choice")=="Random"  && roundCounter>1)
+        {
+            c1Name = p1Manager.GetCharacterName(1);
+            PlayerPrefs.SetString("Player1Choice",c1Name);
+            p1Random = true;
+        }
+
+        if (PlayerPrefs.GetString("Player2Choice")=="Random" && roundCounter>1)
+        {
+            c2Name = p2Manager.GetCharacterName(1);
+            PlayerPrefs.SetString("Player2Choice", c2Name);
+            p2Random = true;
+        }
+
+        if(p1Random && gameEnd)
+        {
+            PlayerPrefs.SetString("Player1Choice", "Random");
+        }
+
+        if (p2Random && gameEnd)
+        {
+            PlayerPrefs.SetString("Player2Choice", "Random");
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
