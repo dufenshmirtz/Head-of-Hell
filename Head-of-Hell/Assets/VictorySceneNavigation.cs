5e9d75af50ee5317c4cbc7fb756ae35909b80557
TheodenKink <arishart10@gmail.com>
2025-03-31 20:20:14 +0300

    Fixed Restart Game
    cancerous restart when space is hit is gone

--- Head-of-Hell/Assets/VictorySceneNavigation.cs
+++ Head-of-Hell/Assets/VictorySceneNavigation.cs
@@ -6,13 +6,34 @@ public class VictoryScreenNavigation : MonoBehaviour
 {
     public Button PlayAgainButton;
     public Button BackToMenuButton;
+    private bool isVictoryScreenActive = false;
 
+    // Initialize the script as disabled (via Inspector checkbox)
     void Start()
     {
-        // Set the first selected button
+        // No need for 'enabled = false' here if you uncheck the script in Inspector
+    }
+
+    // Called when the script is enabled (manually or via code)
+    void OnEnable()
+    {
+        isVictoryScreenActive = true;
+        SetupButtonNavigation();
+    }
+
+    // Called when the script is disabled
+    void OnDisable()
+    {
+        isVictoryScreenActive = false;
+    }
+
+    // Set up button navigation (called in OnEnable)
+    private void SetupButtonNavigation()
+    {
+        // Set default selected button
         EventSystem.current.SetSelectedGameObject(PlayAgainButton.gameObject);
 
-        // Explicitly set navigation
+        // Configure explicit navigation
         Navigation navPlayAgain = PlayAgainButton.navigation;
         navPlayAgain.mode = Navigation.Mode.Explicit;
         navPlayAgain.selectOnRight = BackToMenuButton;
@@ -28,16 +49,14 @@ public class VictoryScreenNavigation : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        if (!isVictoryScreenActive) return;
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             GameObject selected = EventSystem.current.currentSelectedGameObject;
-            if (selected != null)
+            if (selected != null && selected.TryGetComponent(out Button button))
             {
-                Button button = selected.GetComponent<Button>();
-                if (button != null)
-                {
-                    button.onClick.Invoke();
-                }
+                button.onClick.Invoke();
             }
         }
     }
