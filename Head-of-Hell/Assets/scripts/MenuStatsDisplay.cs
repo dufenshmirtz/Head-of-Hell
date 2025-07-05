7f2f7fc84c1cd9934c022525e9142e7c16fab697
TheodenKink <arishart10@gmail.com>
2025-07-05 21:38:31 +0300

    StatsMenuNavigation

--- Head-of-Hell/Assets/scripts/MenuStatsDisplay.cs
+++ Head-of-Hell/Assets/scripts/MenuStatsDisplay.cs
@@ -1,13 +1,85 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class MenuStatsDisplay : MonoBehaviour
 {
-    [SerializeField] private TMP_Text statsDisplayText; // Reference to TextMeshPro component
+    [SerializeField] private TMP_Text statsDisplayText;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private MainMenuMusic sfx;  // Reference to your sound effect script
+
+    private Button[] buttons;
+    private int selectedIndex = 0;
+    private bool notSelected = true;
 
     void Start()
     {
         UpdateStatsDisplay();
+        buttons = new Button[] { resetButton, backButton };
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (notSelected)
+            {
+                AutoSelectButton();
+                return;
+            }
+            Navigate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (notSelected)
+            {
+                AutoSelectButton();
+                return;
+            }
+            Navigate(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            ClickSelectedButton();
+        }
+    }
+
+    void Navigate(int direction)
+    {
+        if (sfx != null)
+            sfx.ButtonSound();
+
+        // Deselect the current button (optional, can trigger OnDeselect callbacks)
+        buttons[selectedIndex].OnDeselect(null);
+
+        selectedIndex += direction;
+        if (selectedIndex < 0) selectedIndex = buttons.Length - 1;
+        else if (selectedIndex >= buttons.Length) selectedIndex = 0;
+
+        EventSystem.current.SetSelectedGameObject(buttons[selectedIndex].gameObject);
+        buttons[selectedIndex].Select();
+    }
+
+    void ClickSelectedButton()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(buttons[selectedIndex].gameObject, pointer, ExecuteEvents.pointerClickHandler);
+    }
+
+    void AutoSelectButton()
+    {
+        if (buttons.Length > 0)
+        {
+            if (sfx != null)
+                sfx.ButtonSound();
+
+            EventSystem.current.SetSelectedGameObject(buttons[0].gameObject);
+            buttons[0].Select();
+            selectedIndex = 0;
+            notSelected = false;
+        }
     }
 
     private void UpdateStatsDisplay()
@@ -18,7 +90,6 @@ public class MenuStatsDisplay : MonoBehaviour
             return;
         }
 
-        // Access the character stats from CharacterStatsManager
         string statsText = "Character Win Ratios:\n";
         foreach (var character in CharacterStatsManager.Instance.GetCharacterStats())
         {
@@ -32,6 +103,6 @@ public class MenuStatsDisplay : MonoBehaviour
     public void ResetStats()
     {
         CharacterStatsManager.Instance.ResetAllStats();
-        UpdateStatsDisplay() ;
+        UpdateStatsDisplay();
     }
 }
