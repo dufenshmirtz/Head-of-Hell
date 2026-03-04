015a0074565b0022e5d9a5842fe11d345e87d187
TheodenKink <arishart10@gmail.com>
2026-03-04 13:27:50 +0200

    UI done

---
+++ Head-of-Hell/Assets/RulesetSelectionState.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class RulesetSelectionState
+{
+    // -1 = Default, 1..5 = Custom slots
+    public static int SelectedSlot = -1;
+
+    public static void SelectDefault()
+    {
+        SelectedSlot = -1;
+    }
+
+    public static void SelectSlot(int slotNumber)
+    {
+        SelectedSlot = slotNumber; // 1..5
+    }
+}
