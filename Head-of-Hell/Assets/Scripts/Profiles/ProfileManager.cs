42dfa4db08d2469500015691dc95fe4325886c6a
TheodenKink <arishart10@gmail.com>
2026-03-04 16:46:04 +0200

    Profiles
    Drop down list in character select DONE
    needs polishing

--- Head-of-Hell/Assets/Scripts/Profiles/ProfileManager.cs
+++ Head-of-Hell/Assets/Scripts/Profiles/ProfileManager.cs
@@ -5,10 +5,16 @@ public class ProfileManager : MonoBehaviour
 {
     public static ProfileManager I { get; private set; }
 
+    private const int NONE = -1;
+    private const int GUEST = -2;
+
     [Header("Runtime")]
     [SerializeField] private ProfilesDatabase db = new ProfilesDatabase();
-    [SerializeField] private int selectedIndexP1 = -1;
-    [SerializeField] private int selectedIndexP2 = -1;
+    [SerializeField] private int selectedIndexP1 = NONE;
+    [SerializeField] private int selectedIndexP2 = NONE;
+
+    [SerializeField] private ProfileData guestProfile;
+
 
     private string SavePath => Path.Combine(Application.persistentDataPath, "profiles.json");
 
@@ -21,6 +27,20 @@ public class ProfileManager : MonoBehaviour
         Load();
     }
 
+    private void EnsureGuest()
+    {
+        if (guestProfile == null || guestProfile.id != "GUEST")
+            guestProfile = ProfileData.CreateGuest();
+    }
+
+    public bool IsGuestSelected(int playerNum)
+    {
+        if (playerNum == 1)
+            return selectedIndexP1 == GUEST;
+        else
+            return selectedIndexP2 == GUEST;
+    }
+
     // --- Public API (θα το καλέσει το UI στο επόμενο βήμα) ---
 
     public ProfilesDatabase GetDatabase() => db;
@@ -30,18 +50,40 @@ public class ProfileManager : MonoBehaviour
 
     public ProfileData GetSelectedProfile(int playerNum)
     {
-        int idx = GetSelectedIndex(playerNum);
-        return db.GetAt(idx);
+        EnsureGuest();
+        int idx = (playerNum == 1) ? selectedIndexP1 : selectedIndexP2;
+
+        if (idx == GUEST)
+            return guestProfile;
+
+        if (idx >= 0)
+            return db.GetAt(idx);
+
+        return null;
     }
 
     public void SelectProfile(int playerNum, int index)
     {
         if (!db.HasAt(index)) return;
 
-        if (playerNum == 1) selectedIndexP1 = index;
-        else selectedIndexP2 = index;
+        if (playerNum == 1) 
+            selectedIndexP1 = index;
+        else 
+            selectedIndexP2 = index;
 
         Save();
+    }
+
+    public void SelectGuest(int playerNum)
+    {
+        if (playerNum == 1)
+            selectedIndexP1 = GUEST;
+        else
+            selectedIndexP2 = GUEST;
+
+        EnsureGuest();
+        Save();
+
     }
 
     public ProfileData CreateOrRenameProfileAt(int index, string newName)
