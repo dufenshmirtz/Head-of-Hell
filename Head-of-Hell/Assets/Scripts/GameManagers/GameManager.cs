d928b711c1b5bd615f9871602433ec78637d06ed
Dufenshmirtz <f.chrysomallis@gmail.com>
2026-03-05 09:46:53 +0200

    portalfix chanchan

--- Head-of-Hell/Assets/Scripts/GameManagers/GameManager.cs
+++ Head-of-Hell/Assets/Scripts/GameManagers/GameManager.cs
@@ -108,7 +108,7 @@ public class GameManager : MonoBehaviour
         if (chanChan)
         {
             //maxHealth = Random.Range(100, 201);
-            portalNumber = Random.Range(0, 5);
+            //portalNumber = Random.Range(0, 5);
             print("..; " + portalNumber);
         }
 
