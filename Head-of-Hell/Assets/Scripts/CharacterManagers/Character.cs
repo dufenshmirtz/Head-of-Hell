97397be4e208779523a5d4485229a677b607f15a
TheodenKink <arishart10@gmail.com>
2026-03-11 15:53:24 +0200

    Fixed wrong winnerID and profile name in Flawless win

--- Head-of-Hell/Assets/Scripts/CharacterManagers/Character.cs
+++ Head-of-Hell/Assets/Scripts/CharacterManagers/Character.cs
@@ -1587,7 +1587,7 @@ public abstract class Character : MonoBehaviour
 
         if (enemy.currHealth == maxHealth)
         {
-            gameManager.RoundEndFlawless(playerNum, P2Name);
+            gameManager.RoundEndFlawless(winnerNum, P2Name);
             KeepStats(P2Name,enemy.GetCharID(), P1Name.text,characterID);
         }
         else if (enemy.currHealth <= 0)
