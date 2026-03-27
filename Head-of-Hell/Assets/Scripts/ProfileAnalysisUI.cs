21d491361fb562106db904791401793a8fa107e6
TheodenKink <arishart10@gmail.com>
2026-03-27 17:40:13 +0200

    Better chart
    new max val =0.4

--- Head-of-Hell/Assets/Scripts/ProfileAnalysisUI.cs
+++ Head-of-Hell/Assets/Scripts/ProfileAnalysisUI.cs
@@ -227,12 +227,15 @@ public class ProfileAnalysisPanelUI : MonoBehaviour
 
             float scale = maxVal * 1.2f; // <-- soft cap
 
+            scale = 0.4f;
+
             combatChart.SetValues(
                 p.aggression_raw / scale,
                 p.defense_raw / scale,
                 (p.mobility_raw / 2f) / scale,
                 p.risk_raw / scale
             );
+           
         }
     }
 }
